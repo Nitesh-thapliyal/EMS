fca5a631abeb198017016a6690307a9e7719913d
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2025-04-12 20:38:33 +0530

    upd

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const TaskList = () => {
   return (
-    <div>TaskList</div>
-  )
-}
+    <div
+      id="TaskList"
+      className="h-[55%] overflow-x-auto flex items-center justify-start gap-5 w-full flex-nowrap mt-10 py-5"
+    >
+      <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-red-500 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 text-sm rounded">High</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Start full stack development</h2>
+        <p className="text-sm mt-2"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quod soluta omnis eos cum laborum facilis perferendis optio cupiditate magni necessitatibus error, autem earum nam neque, consequatur fuga ea possimus!</p>
+      </div>
+      <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-green-500 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 text-sm rounded">High</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Start full stack development</h2>
+        <p className="text-sm mt-2"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quod soluta omnis eos cum laborum facilis perferendis optio cupiditate magni necessitatibus error, autem earum nam neque, consequatur fuga ea possimus!</p>
+      </div>
+      <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 text-sm rounded">High</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Start full stack development</h2>
+        <p className="text-sm mt-2"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quod soluta omnis eos cum laborum facilis perferendis optio cupiditate magni necessitatibus error, autem earum nam neque, consequatur fuga ea possimus!</p>
+      </div>
+      <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-blue-500 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 text-sm rounded">High</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Start full stack development</h2>
+        <p className="text-sm mt-2"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quod soluta omnis eos cum laborum facilis perferendis optio cupiditate magni necessitatibus error, autem earum nam neque, consequatur fuga ea possimus!</p>
+      </div>
+      <div className=" flex-shrink-0 h-full w-[300px] p-5 bg-pink-500 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 text-sm rounded">High</h3>
+          <h4 className="text-sm">20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Start full stack development</h2>
+        <p className="text-sm mt-2"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure quod soluta omnis eos cum laborum facilis perferendis optio cupiditate magni necessitatibus error, autem earum nam neque, consequatur fuga ea possimus!</p>
+      </div>
+    </div>
+  );
+};
 
-export default TaskList
+export default TaskList;
